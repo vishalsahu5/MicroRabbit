002ec2c74812ce9b7b77ea202c3cf2736032e300
Sarthak Sahu <vishalsahu5@gmail.com>
2020-04-28 20:36:27 +0530

    Pending changes

--- MicroRabbit/MicroRabbit.Banking.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MicroRabbit.Banking.Application
-{
-    public class Class1
-    {
-    }
-}
---
+++ MicroRabbit/MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -0,0 +1,20 @@
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Application.Services
+{
+    class AccountService : IAccountService
+    {
+
+        private readonly IAccountRepository _accountRepository
+
+        public IEnumerable<Account> GetAccounts()
+        {
+
+        }
+    }
+}
